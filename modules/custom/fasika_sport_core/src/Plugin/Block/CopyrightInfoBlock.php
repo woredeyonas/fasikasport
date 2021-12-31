5d2e82f4a060ed3fac77c26f9cecdb652e008090
Yonas Legesse <yonas.legesse@axelerant.com>
2021-12-31 10:13:56 +0300

    Defined custom module and copyright block

---
+++ modules/custom/fasika_sport_core/src/Plugin/Block/CopyrightInfoBlock.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\fasika_sport_core\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a block with a simple text.
+ *
+ * @Block(
+ *   id = "copy_right_info",
+ *   admin_label = @Translation("Copyright Info block"),
+ * )
+ */
+class CopyrightInfoBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#type' => 'markup',
+      '#markup' => '<p>&#169; ' . date("Y") . ' ' . $this->t('Fasika Sport') .'</p>',
+    ];
+  }
+
+}
